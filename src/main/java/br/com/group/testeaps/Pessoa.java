57a9444d799a85437494d9c3f6077c26e4a8e4ba
hevellyn <hevellyn.morais@dce.ufpb.br>
2014-06-01 23:16:18 -0300

    att
    att a classe

--- src/main/java/br/com/group/testeaps/Pessoa.java
+++ src/main/java/br/com/group/testeaps/Pessoa.java
@@ -1,5 +1,28 @@
 package br.com.group.testeaps;
 
 public class Pessoa {
+	public int matricula = 0;
+    public String nome = "Hévellyn";
+	
+    
+    
+    public Pessoa (String nome, int idade){
+    	this.nome = nome;
+    	this.matricula = matricula;
+    }
+    
+    public int getMatricula() {
+		return matricula;
+	}
+	public void setMatricula(int novamatricula) {
+		this.matricula = novamatricula;
+	}
+	
+	public String getNome(){
+		return nome;
+	}
+	public void setNome(String novonome){
+		this.nome = novonome;
+	}
 
 }
