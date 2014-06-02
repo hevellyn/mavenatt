c5e53c63c63ff020d85303fefa57b128f7f9ab71
hevellyn <hevellyn.morais@dce.ufpb.br>
2014-06-01 23:18:02 -0300

    att teste
    att o arquivo teste

--- src/test/java/br/com/group/testeaps/AppTest.java
+++ src/test/java/br/com/group/testeaps/AppTest.java
@@ -1,38 +1,26 @@
 package br.com.group.testeaps;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.*;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+
+public class AppTest{ 
+    
+	@Test
+    public void testIdade(){
+        App app = new App();
+		assertTrue(app.isIdadezero());
+		app.setIdade(1);
+		assertFalse(app.isIdadezero());
+		app.setIdade(0);
+		assertTrue(app.isIdadezero());
+		ArrayList<Pessoa> p = new ArrayList<Pessoa>();
+		
+		
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+	
 }
